a0ab93b59d3452d86b8fe25278b605b097bac082
dimbo1324 <dim4dmi7rij@yandex.ru>
2025-12-20 19:04:18 +0300

    Решено: 42. Trapping Rain Water

--- leetcode/arrays/42. Trapping Rain Water/main.py
+++ leetcode/arrays/42. Trapping Rain Water/main.py
@@ -1,5 +1,3 @@
-# https://leetcode.com/problems/trapping-rain-water/description/?envType=problem-list-v2&envId=array
-
 from typing import List
 
 
@@ -7,9 +5,7 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         ans = 0
         distance = len(height)
-        if distance > 3:
-            # ------------------------------------
-            # ------------------------------------
+        if distance >= 3:
             lt, rg = 0, distance - 1
             lt_max, rg_max = 0, 0
             while lt <= rg:
@@ -18,23 +14,35 @@ class Solution:
                         lt_max = height[lt]
                     else:
                         ans += lt_max - height[lt]
-
                     lt += 1
-
                 else:
                     if height[rg] >= rg_max:
                         rg_max = height[rg]
                     else:
                         ans += rg_max - height[rg]
-
                     rg -= 1
-
-            # ------------------------------------
-            # ------------------------------------
-
         return ans
 
 
-arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-# arr = [0, 1, 0, 1]
-print(Solution().trap(arr))
+# ----------------------------------------------
+def main():
+    tests = [
+        [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
+        [4, 2, 0, 3, 2, 5],
+        [2, 0, 2],
+        [0, 0, 0, 0],
+        [5, 4, 3, 2, 1],
+        [1, 2, 3, 4, 5],
+        [3, 0, 0, 2, 0, 4],
+        [1],
+        [],
+    ]
+    solution = Solution()
+    for idx, h in enumerate(tests, start=1):
+        result = solution.trap(h)
+        print(f"Example #{idx} Input: {h} -> Trapped: {result}")
+
+
+# ----------------------------------------------
+if __name__ == "__main__":
+    main()
