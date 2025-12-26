2053feb5fb17b2977b420cb4e3b5c5ed1a8900b4
dimbo1324 <dimaprihodko180@gmail.com>
2025-12-26 14:58:12 +0300

    + задача

---
+++ leetcode/strings/344. Reverse String/golang/main.go
@@ -0,0 +1,14 @@
+package main
+
+func reverseString(s []byte) {
+	n := len(s)
+
+	for i := 0; i < n/2; i++ {
+		s[i], s[n-1-i] = s[n-1-i], s[i]
+	}
+}
+
+func main() {
+	input := []byte{'h', 'e', 'l', 'l', 'o'}
+	reverseString(input)
+}
