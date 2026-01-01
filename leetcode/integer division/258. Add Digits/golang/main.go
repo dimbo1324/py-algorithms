4cbd52bd21d4e768dd280231b7614b5e3d78e6e5
dimbo1324 <dim4dmi7rij@yandex.ru>
2026-01-01 22:20:40 +0300

    + two tasks

---
+++ leetcode/integer division/258. Add Digits/golang/main.go
@@ -0,0 +1,41 @@
+/*
+------------------------------
+------------------------------
+
+Example 1:
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+Example 2:
+Input: num = 0
+Output: 0
+
+------------------------------
+------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func addDigits(num int) int {
+
+	divmod := func(x, y int) (int, int) {
+		return x / y, x % y
+	}
+
+	for num > 9 {
+		x, y := divmod(num, 10)
+		num = x + y
+	}
+	return num
+}
+func main() {
+	n := 38
+	fmt.Println(addDigits(n))
+}
